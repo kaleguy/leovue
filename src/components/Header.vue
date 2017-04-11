5ee5adc241c4a9d8ba4b090d2eb36674969599a9
Joe Orr <joe.orr@gmail.com>
2017-04-11 05:27:03 -0400

    icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="Header">
   Leo Viewer
+    <icon class="icon" name="bars"></icon>
+    <div class="vshim"></div>
+    <icon class="icon" name="arrow-right"></icon>
+    <icon class="icon" name="arrow-left"></icon>
   </div>
 </template>
 
@@ -24,5 +28,15 @@
     padding-left:6px;
     color: #333;
     border-bottom: 1px solid #ddd;
+  }
+  .icon {
+    float: right;
+    padding:3px;
+    padding-right: 7px;
+    color: #333;
+  }
+  .vshim {
+    width: 8px;
+    float: right;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-// import Icon from 'vue-awesome/components/Icon.vue'
-// import 'vue-awesome/icons'
-// Vue.component('icon', Icon)
+import Icon from 'vue-awesome/components/Icon.vue'
+import 'vue-awesome/icons/bars'
+import 'vue-awesome/icons/arrow-left'
+import 'vue-awesome/icons/arrow-right'
+Vue.component('icon', Icon)
 
 Vue.config.productionTip = false
 
