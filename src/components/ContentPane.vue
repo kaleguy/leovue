0f087dbf62469cf87e94beb66812caf0d8ffc1ad
Joe Orr <joe.orr@gmail.com>
2018-11-25 15:26:53 -0500

    dist

--- src/components/ContentPane.vue
+++ src/components/ContentPane.vue
@@ -11,10 +11,12 @@
         </div>
       </div>
       <div id="tlayout" :style="{height: 'calc(100vh - 33px)'}">
-        <div :style="{position:'relative', overflow: 'hidden', width: '100%'}">
-          <div class="inner-container" id="content-inner-container" v-on:scroll="onScroll" style="width:100%">
+        <div id="tpane" :style="{position:'relative', width: '100%'}">
+          <div :style="{width: cpWidth}">
+          <div class="inner-container" id="content-inner-container" v-on:scroll="onScroll">
             <div class="right-cpane" :style="{width: cpWidth}">
               <component :is="dynComponent" v-bind="$props"/>
+            </div>
             </div>
           </div>
         </div>
